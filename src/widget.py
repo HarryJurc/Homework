65b130bb13c0f55441f76fa930adbc907a19b4dc
Igor Gurchenko <igorivanov122@gmail.com>
2024-12-05 20:20:12 +0300

    Added function get_date

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,8 @@
-from masks import get_mask_account, get_mask_card_number
+from .masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(number_with_type: str) -> str:
-    acceptable_types = {'card': ' Visa Platinum , Maestro ', 'account': ' Счет '}
+    acceptable_types = {"card": " Visa Platinum , Maestro ", "account": " Счет "}
     only_number = ""
     only_type = ""
     for symbol in number_with_type:
@@ -9,9 +10,13 @@ def mask_account_card(number_with_type: str) -> str:
             only_number += symbol
         else:
             only_type += symbol
-    if only_type in acceptable_types['account'] and len(only_number) == 20:
+    if only_type in acceptable_types["account"] and len(only_number) == 20:
         return only_type + get_mask_account(int(only_number))
-    elif only_type in acceptable_types['card'] and len(only_number) == 16:
+    elif only_type in acceptable_types["card"] and len(only_number) == 16:
         return only_type + get_mask_card_number(int(only_number))
     else:
         return "Ошибка: Данные введены неверно!"
+
+
+def get_date(date_input: str) -> str:
+    return f'"{date_input[8:10]}.{date_input[5:7]}.{date_input[:4]}"'
