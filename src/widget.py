671a90e83cde91c0f9100e7a86e8b7a5df7fbd8d
Igor Gurchenko <igorivanov122@gmail.com>
2024-12-05 20:04:56 +0300

    Added widget.py with new function

---
+++ src/widget.py
@@ -0,0 +1,17 @@
+from masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(number_with_type: str) -> str:
+    acceptable_types = {'card': ' Visa Platinum , Maestro ', 'account': ' Счет '}
+    only_number = ""
+    only_type = ""
+    for symbol in number_with_type:
+        if symbol.isdigit():
+            only_number += symbol
+        else:
+            only_type += symbol
+    if only_type in acceptable_types['account'] and len(only_number) == 20:
+        return only_type + get_mask_account(int(only_number))
+    elif only_type in acceptable_types['card'] and len(only_number) == 16:
+        return only_type + get_mask_card_number(int(only_number))
+    else:
+        return "Ошибка: Данные введены неверно!"
