16f08ccd46014f681b6c75c695a7319873337ba7
Igor Gurchenko <igorivanov122@gmail.com>
2024-12-14 18:20:06 +0300

    Small fixes

--- src/processing.py
+++ src/processing.py
@@ -4,6 +4,7 @@ from typing import Any
 def filter_by_state(dictionary_list: list, state_value: Any = "EXECUTED") -> list:
     """Функция принимает список словарей и возвращает новый список словарей,
     отбирая их по заданному значению ключа "state" (по-умолчанию: "EXECUTED")"""
+
     new_dictionary_list = []
     for dictionary in dictionary_list:
         if dictionary["state"] == state_value:
@@ -13,4 +14,5 @@ def filter_by_state(dictionary_list: list, state_value: Any = "EXECUTED") -> lis
 
 def sort_by_date(dictionary_list: list, is_reverse: bool = True) -> list:
     """Функция принимает список словарей и сортирует их по датам (по-умолчанию: по убыванию)"""
+
     return sorted(dictionary_list, key=lambda x: x["date"], reverse=is_reverse)
