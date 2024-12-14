e4a6c66c36e92a8bb0336c855ccde59d1277b4ed
Igor Gurchenko <igorivanov122@gmail.com>
2024-12-14 20:13:04 +0300

    Changed type annotations for "dictionary_list"

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 
-def filter_by_state(dictionary_list: list, state_value: Any = "EXECUTED") -> list:
+def filter_by_state(dictionary_list: list[dict], state_value: Any = "EXECUTED") -> list:
     """Функция принимает список словарей и возвращает новый список словарей,
     отбирая их по заданному значению ключа "state" (по-умолчанию: "EXECUTED")"""
 
@@ -12,7 +12,7 @@ def filter_by_state(dictionary_list: list, state_value: Any = "EXECUTED") -> lis
     return new_dictionary_list
 
 
-def sort_by_date(dictionary_list: list, is_reverse: bool = True) -> list:
+def sort_by_date(dictionary_list: list[dict], is_reverse: bool = True) -> list:
     """Функция принимает список словарей и сортирует их по датам (по-умолчанию: по убыванию)"""
 
     return sorted(dictionary_list, key=lambda x: x["date"], reverse=is_reverse)
